993a7690e9767a67ba71c1bf18cff3d12f66e856
Anton Heurlin <Antonheu@kth.se>
2020-03-02 14:58:28 +0100

    AddHotspotFragment implemented

--- native/app/src/main/java/com/leechdev/leech/AddHotspotFragment.java
+++ native/app/src/main/java/com/leechdev/leech/AddHotspotFragment.java
@@ -4,14 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class AddHotspotFragment extends Fragment {
+
+    DbHandler leechDB;
+    EditText editHotspotName, editHotspotLat, editHotspotLong;
+    Button btnAddHotspot, btnGetHotspots;
+
     @NonNull
     @Override
     public View onCreateView(LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_add_hotspot, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_hotspot, container, false);
+
+        leechDB = new DbHandler(getActivity().getApplicationContext());
+
+        editHotspotName = view.findViewById(R.id.HotspotName);
+        editHotspotLat = view.findViewById(R.id.HotspotLat);
+        editHotspotLong = view.findViewById(R.id.HotspotLong);
+        btnAddHotspot = view.findViewById(R.id.buttonAddHospot);
+        btnGetHotspots = view.findViewById(R.id.buttonGetHotspot);
+        AddHotspot();
+
+        return view;
+        //// inflater.inflate(R.layout.fragment_add_hotspot, container, false);
+    }
+
+    public void AddHotspot(){
+        btnAddHotspot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean insertResult = leechDB.addHotspot(new Hotspot(editHotspotName.getText().toString(), editHotspotLat.getText().toString(), editHotspotLong.getText().toString()));
+
+                if(insertResult == true){
+                    Toast.makeText(getActivity().getApplicationContext(), "data inserted", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getActivity().getApplicationContext(), "data not inserted", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- native/app/src/main/java/com/leechdev/leech/DbHandlerTesting.java
+++ native/app/src/main/java/com/leechdev/leech/DbHandlerTesting.java
@@ -1,5 +1,6 @@
 package com.leechdev.leech;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,11 +9,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class DbHandlerTesting extends AppCompatActivity {
 
     DbHandler leechDB;
     EditText editHotspotName, editHotspotLat, editHotspotLong;
-    Button btnAddHotspot;
+    Button btnAddHotspot, btnGetHotspots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +27,9 @@ public class DbHandlerTesting extends AppCompatActivity {
         editHotspotLat = findViewById(R.id.HotspotLat);
         editHotspotLong = findViewById(R.id.HotspotLong);
         btnAddHotspot = findViewById(R.id.buttonAddHospot);
+        btnGetHotspots = findViewById(R.id.buttonGetHotspot);
         AddHotspot();
+        getAllHotspots();
     }
 
     public void AddHotspot(){
@@ -40,6 +45,38 @@ public class DbHandlerTesting extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void getAllHotspots(){
+        btnGetHotspots.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ArrayList<Hotspot> hotspotList = leechDB.getAllHotspots();
+                        StringBuffer buffer = new StringBuffer();
+                        if(hotspotList.isEmpty()){
+                            showMessage("error", "no data found");
+                            return;
+                        }else{
+                            showMessage("success", "data found");
+                            for(Hotspot hotspot : hotspotList){
+                                buffer.append("Hotspot name: " +hotspot.getHotspot_name() +"\n");
+                                buffer.append("Hotspot latitude: " +hotspot.getHotspot_lat() +"\n");
+                                buffer.append("Hotspot longitude: " +hotspot.getHotspot_long() +"\n");
+                            }
+                            showMessage("Data", buffer.toString());
+                        }
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 
 }
